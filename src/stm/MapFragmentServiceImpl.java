527e6da7b5a5ea09a3391ba5025b73c771d94740
axel <formella.alex@gmail.com>
2019-12-11 21:57:56 +0100

    Working image reading

--- src/stm/MapFragmentServiceImpl.java
+++ src/stm/MapFragmentServiceImpl.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Base64;
 
 @WebService(endpointInterface = "stm.MapFragmentService", targetNamespace = "stmMapa")
@@ -13,15 +14,15 @@ public class MapFragmentServiceImpl implements MapFragmentService {
 
     @Override
     public String getMapFragment(int x1, int y1, int x2, int y2) throws IOException {
-//        BufferedImage mapFragment = ImageIO
-//                .read(new File("resources/zakopane.png"))
-//                .getSubimage(x1, y1, x2, y2);
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        ImageIO.write(mapFragment, "png", baos );
-//        baos.flush();
-//        byte[] mapFragmentBytes = baos.toByteArray();
-//        baos.close();
-//        return Base64.getEncoder().encodeToString(mapFragmentBytes);
-        return "TU POWINIEN BYC BASE64";
+        InputStream inputStream = MapFragmentServiceImpl.class.getResourceAsStream("/zakopane.png");
+        BufferedImage mapFragment = ImageIO
+                .read(inputStream)
+                .getSubimage(x1, y1, x2, y2);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(mapFragment, "png", baos );
+        baos.flush();
+        byte[] mapFragmentBytes = baos.toByteArray();
+        baos.close();
+        return Base64.getEncoder().encodeToString(mapFragmentBytes);
     }
 }
